90ea63ccff0505bcb860be504f9932e9c70baaa0
ChingHengWang <qoogood1234@gmail.com>
2016-08-05 17:24:47 +0800

    update github

--- github_config.sh
+++ github_config.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+git config --global user.email "qoogood1234@gmail.com" 
+git config --global user.name "ChingHengWang"
+
 git config --global credential.helper 'cache --timeout=604800'
 #7 days
