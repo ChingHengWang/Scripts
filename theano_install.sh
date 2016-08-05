cdd59f218cd97f2c44d8babe656c326ed234d7e4
ChingHengWang <qoogood1234@gmail.com>
2016-08-05 17:18:51 +0800

    theano_install update

--- theano_install.sh
+++ theano_install.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+THEANO=RUN
+DOCKER=RUN
+
+if [ $THEANO == "RUN" ]
+then
 sudo pip install Theano
+fi
+
+if [ $DOCKER == "RUN" ]
+then
+sudo docker run -it kaixhin/theano
+sudo docker run -it --device /dev/nvidiactl --device /dev/nvidia-uvm --device /dev/nvidia0 kaixhin/cuda-theano:7.0
+fi
+
+
